579f5f2ffc242923dcbacf463230c94e60738d88
Midhun K Jayan <midhunkj333@gmail.com>
2026-04-03 21:13:47 +0530

    deployment changes

--- bails_arena/settings.py
+++ bails_arena/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!!e=#dq2c2xh=8m_e=h0to0wel7=p&p^0-br+jcdw)b&b@0h#z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
