57d73221e47bb7fb932088bd41f3ff45103c7be3
Midhun K Jayan <midhunkj333@gmail.com>
2026-04-08 21:49:51 +0530

    moved to postgres

--- bails_arena/settings.py
+++ bails_arena/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,13 +78,15 @@ WSGI_APPLICATION = 'bails_arena.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgresql://bailsarena:oV5dfcblSp51zvYx8XQvZnkVcRkKqhlW@dpg-d7b7q3fafjfc739fffhg-a/bailsarena_db')
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
